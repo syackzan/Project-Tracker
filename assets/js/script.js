95c75abf799581aa8b73cd1ef95d862fee7002cf
Scotty Yackzan <syackzan@gmail.com>
2021-08-19 12:49:54 -0700

    Added more user input into Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 console.log("hello");
 
-$('#modal').modal(options)
+//Storing HTML Elements to Variables//
+var modalB = $("#modalB");
+var closeM = $(".close");
+
+//Onclick Event to Show Modal//
+modalB.on("click", openModal);
+closeM.on("click", closeModal);
+
+//Function to Show Modal//
+function openModal(){
+    $(function () {
+        $('#modalOne').modal("show");
+    });
+}
+
+//Function to Exit out of Modal//
+function closeModal(){
+    $(function () {
+        $('#modalOne').modal("hide");
+    });
+}
